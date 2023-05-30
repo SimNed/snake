101d921ff4971552f66126f448d9e9e4e1462cf0
SimNed <simon.nedjari@outlook.com>
2023-05-30 15:50:35 +0200

    add controls and freeze movement axis to -axis

--- constants.lua
+++ constants.lua
@@ -1,10 +1,14 @@
 local CONSTANTS = {
     GRID_SIZE = 35,
     WINDOW_SCALE = 10,
+    DEFAULT_REFRESH_DELAY = .2,
     SNAKE_DEFAULT_SIZE = 4,
     SNAKE_DEFAULT_POSITION = {x = (35 - 1) / 2, y = (35 - 1) / 2},
     SNAKE_DEFAULT_DIRECTION = {x = 0, y = -1},
     FREE_ID = 0,
+    CHICK_ID = 1,
+    OBSTACLE_ID = 2,
+    SNAKE_ID = 3,
 }
 
 return CONSTANTS
--- main.lua
+++ main.lua
@@ -1,9 +1,6 @@
 local constants = require 'constants'
 local initializers = require 'initializers'
 
-local grid = initializers.init_grid(constants.GRID_SIZE, constants.FREE_ID)
-local snake = initializers.init_snake(constants.SNAKE_DEFAULT_SIZE, constants.SNAKE_DEFAULT_POSITION, constants.SNAKE_DEFAULT_DIRECTION)
-
 local game = {
     state = {
         start_menu = true,
@@ -12,16 +9,24 @@ local game = {
     }
 }
 
-function move_snake(snake, direction)
+local grid = initializers.init_grid(constants.GRID_SIZE, constants.FREE_ID)
+local snake = initializers.init_snake(constants.SNAKE_DEFAULT_SIZE, constants.SNAKE_DEFAULT_POSITION, constants.SNAKE_DEFAULT_DIRECTION)
+
+local direction = constants.SNAKE_DEFAULT_DIRECTION
+local tempDirection = direction
+local refreshDelay = constants.DEFAULT_REFRESH_DELAY
+local timer = 0
+
+function move_snake(cellPositions, direction)
     local tempPosition = {}
     local targetPosition = {
-        x = snake[1].x + direction.x, 
-        y = snake[1].y + direction.y
+        x = cellPositions[1].x + direction.x, 
+        y = cellPositions[1].y + direction.y
     }
 
-    for i = 1, #snake do
-        tempPosition = snake[i]    
-        snake[i] = targetPosition
+    for i = 1, #cellPositions do
+        tempPosition = cellPositions[i]    
+        cellPositions[i] = targetPosition
         targetPosition = tempPosition
     end
 end
@@ -31,7 +36,25 @@ function love.load()
 end
 
 function love.update(dt)
-    move_snake(snake, {x = 0, y = -1})
+    if love.keyboard.isDown("left") then
+        tempDirection = {x = -1, y = 0}
+      elseif love.keyboard.isDown("right") then
+        tempDirection = {x = 1, y = 0}
+      elseif love.keyboard.isDown("up") then
+        tempDirection = {x = 0, y = -1}
+      elseif love.keyboard.isDown("down") then
+        tempDirection = {x = 0, y = 1}
+    end
+
+    timer = timer + dt
+
+    if timer > refreshDelay then
+        if (direction.x ~= 0 and tempDirection.x ~= direction.x * -1) or (direction.y ~= 0 and tempDirection.y ~= direction.y * -1)  then
+            direction = tempDirection
+        end
+        move_snake(snake, direction)
+        timer = 0
+    end
 end
 
 function love.draw()
